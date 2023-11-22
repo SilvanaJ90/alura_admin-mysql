58592f0c397d5e56549094cfd932db10477d2e58
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-11-22 08:38:54 -0500

    create file consultas_avanzadas

--- comandos_clase3.sql
+++ comandos_clase3.sql
@@ -44,3 +44,109 @@ Renombrar las tablas
 
 USE ventas_jugos;
 ALTER TABLE tb_venta RENAME tb_factura;
+
+
+-- insertar registros
+
+-- un solo registro 
+
+INSERT INTO tb_producto (CODIGO, DESCRIPCION, SABOR, TAMANO, ENVASE, PRECIO_LISTA) VALUES ('1040107', 'Light', 'Sandía', '350 ml', 'Lata', 4.56);
+
+-- mas de un registro
+INSERT INTO tb_producto VALUES 
+    -> ('1040109', 'Light', 'Asaí', '350 ml', 'Lata', 5.60), 
+    -> ('1040110', 'Light', 'Manzana', '350 ml', 'Lata', 6.00),
+    -> ('1040111', 'Light', 'Mango', '350 ml', 'Lata', 3.50);
+
+
+-- insertar varios registros usando una consulta
+
+--consulta 
+SELECT CODIGO_DEL_PRODUCTO AS CODIGO, NOMBRE_DEL_PRODUCTO AS DESCRIPCION, SABOR, TAMANO, ENVASE, PRECIO_DE_LISTA AS PRECIO_LISTA  FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO NOT IN (SELECT
+CODIGO FROM tb_producto);
+
+INSERT INTO tb_producto SELECT CODIGO_DEL_PRODUCTO AS CODIGO, NOMBRE_DEL_PRODUCTO AS DESCRIPCION, SABOR, TAMANO, ENVASE, PRECIO_DE_LISTA AS PRECIO_LISTA  FROM tabla_de_productos WHERE CODIGO_DEL_PRODUCTO NOT IN (SELECT CODIGO FROM tb_producto);
+
+-- Alterar tablas
+
+UPDATE tb_producto SET PRECIO_LISTA= 5 WHERE CODIGO = '1000889';
+
+-- por lote
+
+UPDATE tb_producto SET DESCRIPCION= 'Sabor de la Montaña',
+TAMANO= '1 Litro', ENVASE = 'Botella PET' WHERE 
+CODIGO = '1000889';
+
+-- basado en el mismo campo
+SELECT * FROM tb_cliente;
+UPDATE tb_cliente SET VOLUMEN_COMPRA = VOLUMEN_COMPRA/10;
+
+-- basado en los campos de otras tablas
+
+
+UPDATE tb_vendedor A
+INNER JOIN
+tabla_de_vendedores B
+ON A.MATRICULA = SUBSTRING(B.MATRICULA,3,3)
+SET A.DE_VACACIONES = B.VACACIONES;
+
+-- eliminar registros
+DELETE FROM tb_producto WHERE TAMANO = '1 Litro';
+
+-- eliminar registros utilizando seleccion de datos
+
+DELETE FROM tb_producto
+WHERE CODIGO NOT IN (SELECT CODIGO_DEL_PRODUCTO 
+FROM tabla_de_productos);
+
+
+
+Trigger
+
+DELIMITER //
+CREATE TRIGGER TG_FACTURACION_INSERT 
+AFTER INSERT ON tb_items_facturas1
+FOR EACH ROW BEGIN
+  DELETE FROM tb_facturacion;
+  INSERT INTO tb_facturacion
+  SELECT A.FECHA, SUM(B.CANTIDAD * B.PRECIO) AS VENTA_TOTAL
+  FROM tb_factura1 A
+  INNER JOIN
+  tb_items_facturas1 B
+  ON A.NUMERO = B.NUMERO
+  GROUP BY A.FECHA;
+END //
+
+
+
+DELIMITER //
+CREATE TRIGGER TG_FACTURACION_DELETE
+AFTER DELETE ON tb_items_facturas1
+FOR EACH ROW BEGIN
+  DELETE FROM tb_facturacion;
+  INSERT INTO tb_facturacion
+  SELECT A.FECHA, SUM(B.CANTIDAD * B.PRECIO) AS VENTA_TOTAL
+  FROM tb_factura1 A
+  INNER JOIN
+  tb_items_facturas1 B
+  ON A.NUMERO = B.NUMERO
+  GROUP BY A.FECHA;
+END //
+
+DELIMITER //
+CREATE TRIGGER TG_FACTURACION_UPDATE
+AFTER UPDATE ON tb_items_facturas1
+FOR EACH ROW BEGIN
+  DELETE FROM tb_facturacion;
+  INSERT INTO tb_facturacion
+  SELECT A.FECHA, SUM(B.CANTIDAD * B.PRECIO) AS VENTA_TOTAL
+  FROM tb_factura1 A
+  INNER JOIN
+  tb_items_facturas1 B
+  ON A.NUMERO = B.NUMERO
+  GROUP BY A.FECHA;
+END //
+
+
+
+
